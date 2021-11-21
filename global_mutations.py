3e3ee6207d48abbe629ece891763335307159d78
Samyak Jain <samyakjain02@gmail.com>
2021-11-21 12:57:42 +0530

    init

---
+++ global_mutations.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pylab as plt
+from scipy.integrate import odeint
+import ternary
+e = 0.9
+u = 0.051
+def F(vec, t):
+    x, y = vec
+    x_dot = x - e*(x**3) - e*(y**2)*x + (e-1)*(x**2) - 2*x*y - e*(x**2)*y + u*(1-3*x)
+    y_dot = (-1)*e*(y**3) + (2*e + 1)*(y**2) - e*x*(y**2) - e*(x**2)*y + (2*e + 2)*x*y - (e+1)*y + u*(1-3*y)
+    return [x_dot, y_dot]
+vec0 = [0.5,0.4]
+t = np.linspace(0, 1000, 100000)
+sol = odeint(F, vec0, t)
+# x, y = sol
+# sol = x, y, 1 - x - y
+sol2 = []
+for x, y in sol:
+    sol2.append([x, y, 1-x-y])
+
+#for v in vec0:
+    #sol = odeint(F, v, t)
+    #plt.plot(sol[:, 0], sol[:, 1])
+
+figure, tax = ternary.figure(scale = 1.0)
+tax.boundary()
+tax.gridlines(multiple = 0.2, color = "black")
+tax.set_title("Global Mutations", fontsize = 20)
+tax.plot(sol2, linewidth = 2.0, label = "trajectory")
+tax.ticks(axis = 'lbr', multiple = 0.2, linewidth = 1, tick_formats = "%.1f")
+tax.legend()
+tax.show()
---
+++ single_mutations.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pylab as plt
+from scipy.integrate import odeint
+import ternary
+e = 0.1
+u = 0.03
+def F(vec, t):
+    x, y = vec
+    f_x = 1 - x - (e +2)*y
+    f_y = (e + 2)*x + (e + 1)*(y - 1)
+    phi = e*(x**2 + y**2 - x - y + x*y)
+    x_dot = x*(f_x - phi) - u*x
+    y_dot = y*(f_y - phi) + u*x
+    return [x_dot, y_dot]
+vec0 = [0.8,0]
+t = np.linspace(0, 1000, 10000)
+sol = odeint(F, vec0, t)
+# x, y = sol
+# sol = x, y, 1 - x - y
+sol2 = []
+for x, y in sol:
+    sol2.append([x, y, 1-x-y])
+
+#for v in vec0:
+    #sol = odeint(F, v, t)
+    #plt.plot(sol[:, 0], sol[:, 1])
+
+figure, tax = ternary.figure(scale = 1.0)
+tax.boundary()
+tax.gridlines(multiple = 0.2, color = "black")
+tax.set_title("Single Mutations", fontsize = 20)
+tax.plot(sol2, linewidth = 2.0, label = "trajectory")
+tax.ticks(axis = 'lbr', multiple = 0.2, linewidth = 1, tick_formats = "%.1f")
+tax.legend()
+tax.show()
