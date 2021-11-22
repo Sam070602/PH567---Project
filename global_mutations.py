17df4dc1b171ad67026e4d4bf35527153c05d700
Samyak Jain <samyakjain02@gmail.com>
2021-11-22 21:27:36 +0530

    final codes

--- global_mutations.py
+++ global_mutations.py
@@ -3,14 +3,14 @@ import matplotlib.pylab as plt
 from scipy.integrate import odeint
 import ternary
 e = 0.9
-u = 0.051
+u = 0.05 #u can be varied around epsilon/18, which is the critical point
 def F(vec, t):
     x, y = vec
     x_dot = x - e*(x**3) - e*(y**2)*x + (e-1)*(x**2) - 2*x*y - e*(x**2)*y + u*(1-3*x)
     y_dot = (-1)*e*(y**3) + (2*e + 1)*(y**2) - e*x*(y**2) - e*(x**2)*y + (2*e + 2)*x*y - (e+1)*y + u*(1-3*y)
     return [x_dot, y_dot]
 vec0 = [0.5,0.4]
-t = np.linspace(0, 1000, 100000)
+t = np.linspace(0, 1000, 10000)
 sol = odeint(F, vec0, t)
 # x, y = sol
 # sol = x, y, 1 - x - y
@@ -25,7 +25,7 @@ for x, y in sol:
 figure, tax = ternary.figure(scale = 1.0)
 tax.boundary()
 tax.gridlines(multiple = 0.2, color = "black")
-tax.set_title("Global Mutations", fontsize = 20)
+tax.set_title("No Mutations", fontsize = 20)
 tax.plot(sol2, linewidth = 2.0, label = "trajectory")
 tax.ticks(axis = 'lbr', multiple = 0.2, linewidth = 1, tick_formats = "%.1f")
 tax.legend()
--- no_mutations.py
+++ no_mutations.py
@@ -2,17 +2,14 @@ import numpy as np
 import matplotlib.pylab as plt
 from scipy.integrate import odeint
 import ternary
-e = 0.1 
-u = 0.04 #u can be varied around the critical point (approximately 0.035)
+e = 0 #epsilon can be varied aroun 0, which is the critical point
+u = 0
 def F(vec, t):
     x, y = vec
-    f_x = 1 - x - (e +2)*y
-    f_y = (e + 2)*x + (e + 1)*(y - 1)
-    phi = e*(x**2 + y**2 - x - y + x*y)
-    x_dot = x*(f_x - phi) - u*x
-    y_dot = y*(f_y - phi) + u*x
+    x_dot = x - e*(x**3) - e*(y**2)*x + (e-1)*(x**2) - 2*x*y - e*(x**2)*y + u*(1-3*x)
+    y_dot = (-1)*e*(y**3) + (2*e + 1)*(y**2) - e*x*(y**2) - e*(x**2)*y + (2*e + 2)*x*y - (e+1)*y + u*(1-3*y)
     return [x_dot, y_dot]
-vec0 = [0.8,0]
+vec0 = [0.5,0.4]
 t = np.linspace(0, 1000, 10000)
 sol = odeint(F, vec0, t)
 # x, y = sol
@@ -28,9 +25,8 @@ for x, y in sol:
 figure, tax = ternary.figure(scale = 1.0)
 tax.boundary()
 tax.gridlines(multiple = 0.2, color = "black")
-tax.set_title("Single Mutations", fontsize = 20)
+tax.set_title("No Mutations", fontsize = 20)
 tax.plot(sol2, linewidth = 2.0, label = "trajectory")
 tax.ticks(axis = 'lbr', multiple = 0.2, linewidth = 1, tick_formats = "%.1f")
 tax.legend()
 tax.show()
-
